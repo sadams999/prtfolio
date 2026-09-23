5196d4f92c5c9d46736e1856c0e45a1607329ec4
sadams999 <soncera.dadams@gmail.com>
2026-09-23 16:44:42 -0400

    Update wargames.js

--- wargames.js
+++ wargames.js
@@ -309,7 +309,7 @@ function draw() {
     userStartAudio();
     screen = 1;
     introVid.show();
-    introVid.play().catch(err => console.error('introVid play blocked:', err));
+    introVid.elt.play().catch(err => console.error('introVid play blocked:', err));
     introPlayed = true;
   }
   if (screen == 1){
